5859cdf81252b2de617c66b0899d4dbd57a3edb3
Kamal <kamal.zyel@gmail.com>
2021-03-10 23:34:57 +0700

    limit show app based on breakpoints

--- public/app.js
+++ public/app.js
@@ -106,83 +106,106 @@ var company = new Vue({
         items: [
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/5soq14P4c7WzOGqlOgksDg/602128ee28b589b176f902e8403fe3d3/intercom-logo_2x.png',
-                name: 'Intercom'
+                name: 'Intercom',
+                hiddenOn: 'flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/4T7ntXxswrr0dV9akRfASx/335e4700adb1488a9a4de0b482ce9cc8/salesforces_2x.png',
-                name: 'Salesforces'
+                name: 'Salesforces',
+                hiddenOn: 'flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/4hvpm3NEV6UaSn3mYDEcA7/d28b81dc0a873b09b363d6d14e843da6/zendesk-logo_2x.png',
-                name: 'Zendesk'
+                name: 'Zendesk',
+                hiddenOn: 'flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/5A3E4mdRZFZnMUwTcqvg89/a9cb63ba7ee052407ac866832f946e1a/hubspot-logo_2x.png',
-                name: 'Hubspot'
+                name: 'Hubspot',
+                hiddenOn: 'flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/3T5JRG62JDJ1kCzP05qaMK/9e41e4e4b304d7a94396a78b800b02cb/ms-logo_2x.png',
-                name: 'Microsoft Dynamic'
+                name: 'Microsoft Dynamic',
+                hiddenOn: 'flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/1akwWesL0Pf904skeSqs8B/30c550978c750c41a7a2c3587997de05/shopify_2x.png',
-                name: 'Shopify'
+                name: 'Shopify',
+                hiddenOn: 'flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/69eUHjvL82vSH7NeTsXWft/a8952f89a6e95ceba4c55cd101455b91/gorgias_2x.png',
-                name: 'Gorgias'
+                name: 'Gorgias',
+                hiddenOn: 'hidden md:flex'
+                
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/6Ky6pgY6ykdZfbOZgXIwWV/9cd8afd0eaf61d89f67f73540e277f80/help-scout-logo_2x.png',
-                name: 'Help Scout'
+                name: 'Help Scout',
+                hiddenOn: 'hidden md:flex'
+                
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/1Ki9tYAY45fQLcfZavUxyu/425f79640ce888a39c65d263c6a53019/zapier_2x.png',
-                name: 'Zapier'
+                name: 'Zapier',
+                hiddenOn: 'hidden md:flex'
+                
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/1A0Jjuh4gneIv3HvwtinCg/f2764f80e200848f6c9bf9f881da4ffb/front.png',
-                name: 'Front'
+                name: 'Front',
+                hiddenOn: 'hidden md:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/7yPXIKlYBHlWil6vCi07RG/3995ad4c2e11db6bfe9571cd371ea06a/slack_2x.png',
-                name: 'Slack'
+                name: 'Slack',
+                hiddenOn: 'hidden md:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/ETTSmNuX2w6vm1eeLx6pV/0df4057a9d4f11f2bcfb2bfcb5328979/kustomer_2x.png',
-                name: 'Kustomer'
+                name: 'Kustomer',
+                hiddenOn: 'hidden md:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/5yS1Pd5bfSPMYxv9lrvrXG/f7d6bf00b5e188ee754ac651a0769964/zohocrm-icon.png',
-                name: 'Zoho CRM'
+                name: 'Zoho CRM',
+                hiddenOn: 'hidden lg:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/3tjWztEIyTR0AO4Ltb1JcR/3cd5a03918ac98fb49565bf582909f0a/freshdesk_2x.png',
-                name: 'Freshdesk'
+                name: 'Freshdesk',
+                hiddenOn: 'hidden lg:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/75H8dAqPJ56VOLVX4T6NvP/9a4677f2f4bd414bc21e70717e8a6b14/HappyFox_logo2w.png',
-                name: 'HappyFox'
+                name: 'HappyFox',
+                hiddenOn: 'hidden lg:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/5QvVSXNvekQdRXHVKRyJIG/32c173706d24b4aac2849749febb48f2/JobAdder_2x.png',
-                name: 'JobAdder'
+                name: 'JobAdder',
+                hiddenOn: 'hidden lg:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/73mjiKSkWhObeTDKIcxuqc/a6b126cec0ada32d83d1d0f3f302009f/copper_2x.png',
-                name: 'Copper'
+                name: 'Copper',
+                hiddenOn: 'hidden lg:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/6KLiFcWSWHClMHhDA38Geu/b5933f506428e4de6633282ca74ac3cc/pipedrive_2x.png',
-                name: 'Pipedrive'
+                name: 'Pipedrive',
+                hiddenOn: 'hidden lg:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/6sp7zjpO0puvzgKRv8b4m5/244f15cec294936f00473f01109051b9/no-crm-io_2x.png',
-                name: 'noCRM.io'
+                name: 'noCRM.io',
+                hiddenOn: 'hidden lg:flex'
             },
             {
                 photoURL: 'https://images.ctfassets.net/0kz5nrpxuzuo/4rxldWPldtYH7RnjKKjUmA/ed6362e5d965778703ea43f0406be38a/reamaze-icon.png',
-                name: 'Re:amaze'
+                name: 'Re:amaze',
+                hiddenOn: 'hidden lg:flex'
             },
         ]
     }
